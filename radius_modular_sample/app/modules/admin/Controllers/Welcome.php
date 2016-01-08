42e67785a4fe78df476a17de8ecc149fbb0f0498
Dipen Lama <lamadipen@yahoo.com>
2016-01-08 17:09:43 +0545

    sample prject

---
+++ radius_modular_sample/app/modules/admin/Controllers/Welcome.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Modules\Admin\Controllers;
+
+use App\User;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class Welcome extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function show()
+    {
+         return view("admin::test");
+    }
+}
---
+++ radius_modular_sample/config/module.php
@@ -0,0 +1,11 @@
+<?php
+
+    return [
+        'modules'=>array(
+            "admin",
+            "applicant",
+            "employer",
+            "home",
+            "auth"
+        ),
+    ];
